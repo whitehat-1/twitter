b9e1459582b944cf89e59c4313e2d315faa51cdb
M J <69307802+whitehat-1@users.noreply.github.com>
2022-05-16 00:56:05 +0100

    new commit

---
+++ twitter/server.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+from library.database.database import create_start_app_handler
+# from routers.auth import router as AuthRouter
+# from routers.content import router as ContentRouter
+
+
+def get_application():
+
+    # start the application.
+    app = FastAPI()
+
+    # Connect to database.
+    app.add_event_handler("startup", create_start_app_handler(app))
+    # app.include_router(AuthRouter)
+    # app.include_router(ContentRouter)
+
+    return app
+
+
+app = get_application()
+
+
+@app.get("/")
+async def home():
+    return {"detail": "hello world!"}
